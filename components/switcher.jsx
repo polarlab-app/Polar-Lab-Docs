0f9009bd31c49751e13ee32c6d4f313490eda989
Aertic <63727339+Aertic@users.noreply.github.com>
2024-10-24 22:32:46 +0200

    Add functional doc switching

--- components/switcher.jsx
+++ components/switcher.jsx
@@ -1,28 +1,39 @@
-'use client';
 import styles from '@/src/css/docs/switcher.module.scss';
 import { useTheme } from './themeProvider';
-import findPageDocs from '@/lib/findPageDocs';
 import { useEffect, useState } from 'react';
+import findPageDocs from '@/lib/findPageDocs';
+import { useRouter } from 'next/navigation';
 
 export default function Switcher({ params }) {
-	const { theme } = useTheme();
+    const { theme } = useTheme();
+    const router = useRouter();
+    const [docs, setDocs] = useState({ next: null, prev: null });
 
-	return (
-		<div className={`${styles.container} ${styles[theme]}`}>
-			<div className={styles.button}>
-				<i className={`${styles.icon} icon-angle-up`}></i>
-				<div className={styles.textContainer}>
-					<p className={styles.text}>Commands</p>
-					<p className={styles.text}>Last Page</p>
-				</div>
-			</div>
-			<div className={styles.button}>
-				<div className={styles.textContainer}>
-					<p className={styles.text}>Commands</p>
-					<p className={styles.text}>Next Page</p>
-				</div>
-				<i className={`${styles.icon} icon-angle-up`}></i>
-			</div>
-		</div>
-	);
+    useEffect(() => {
+        const fetchDocs = async () => {
+            const data = await findPageDocs({ path: params });
+            setDocs(JSON.parse(data));
+            console.log(JSON.parse(data));
+        };
+        fetchDocs();
+    }, [params]);
+
+    return (
+        <div className={`${styles.container} ${styles[theme]}`}>
+            <div className={styles.button} onClick={() => router.push('/' + docs.prev.path)}>
+                <i className={`${styles.icon} icon-angle-up`}></i>
+                <div className={styles.textContainer}>
+                    <p className={styles.text}>Commands</p>
+                    <p className={styles.text}>Last Page</p>
+                </div>
+            </div>
+            <div className={styles.button} onClick={() => router.push('/' + docs.next.path)}>
+                <div className={styles.textContainer}>
+                    <p className={styles.text}>Commands</p>
+                    <p className={styles.text}>Next Page</p>
+                </div>
+                <i className={`${styles.icon} icon-angle-up`}></i>
+            </div>
+        </div>
+    );
 }
